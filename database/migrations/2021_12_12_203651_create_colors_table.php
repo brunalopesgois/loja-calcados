9c4ba013edf7e2166984b0ebb644ef09cc877cd7
Bruna Lopes <bruna-chan@live.com>
2021-12-12 17:39:09 -0300

    refactor: adjust products table to be possible only one color

--- database/migrations/2021_12_12_203651_create_colors_table.php
+++ database/migrations/2021_12_12_203651_create_colors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
+class CreateColorsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,9 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('colors', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->integer('lot_id');
-            $table->integer('color_id')->unique();
-            $table->string('description');
-
-            $table->foreign('lot_id')->references('id')->on('lots');
-            $table->foreign('color_id')->references('id')->on('colors');
         });
     }
 
@@ -32,6 +26,6 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('colors');
     }
 }
